e48b75a029a3c2179488c13fae2e39278a37b2f5
AngheoLeivaC <angheloleivacapritan@gmail.com>
2024-11-11 15:27:49 -0500

    Lista de productos agregar y eliminar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ListaTareasApp';
+export class AppComponent implements OnInit {
+  listaTareas: string[] = [];
+  nuevaTrarea: string = '';
+
+  private _tareasService = inject(TareasService)
+
+  ngOnInit(): void {
+    this.listaTareas = this. _tareasService.getTareas();
+  }
+
+  agregarTarea(){
+    this._tareasService.agregarTarea(this.nuevaTrarea)  //agregamos una tarea mediante el servicio
+    this.nuevaTrarea = '';                              // vaciamos el imput donde escribimos la tarea
+    this.listaTareas = this._tareasService.getTareas()  // volvemoa a traer la lista de tareas para mostrar todas
+  }
+
+  eliminarTarea(index: number) {                        // pasamos el id
+    this._tareasService.eliminarTarea(index);           // elminamos la tarea del id llamado
+    this.listaTareas = this._tareasService.getTareas()  // llamos a la lista de tareas actualizada
+  }
 }
